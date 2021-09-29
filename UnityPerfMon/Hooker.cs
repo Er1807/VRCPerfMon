db676cdef47ee0e0d4b6728c1e4405b80217919f
Eric van Fandenfart <ruhlanderic@gmail.com>
2021-09-29 12:22:25 +0200

    init

---
+++ UnityPerfMon/Hooker.cs
@@ -0,0 +1,118 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UnhollowerBaseLib;
+using UnityEngine;
+using UnityEngine.XR;
+using VRC;
+using VRC.Udon;
+
+namespace UnityPerfMon
+{
+    public class Hooker
+    {
+        class Item
+        {
+
+            DateTime current { get; set; } = DateTime.Now;
+            int framecount { get; set; } = Time.frameCount;
+            string type { get; set; }
+            string user { get; set; }
+            string objectname { get; set; }
+            TimeSpan TimeTook { get; set; }
+
+            public Item(string type, string user, string objectname, TimeSpan timeTook)
+            {
+                this.type = type;
+                this.user = user;
+                this.objectname = objectname;
+                TimeTook = timeTook;
+            }
+
+            public override string ToString()
+            {
+                Dictionary<string, System.Object> dic = new Dictionary<string, System.Object>() {
+                    {"current",current },
+                    {"framecount",framecount },
+                    {"type",type },
+                    {"user",user },
+                    {"objectname",objectname },
+                    {"TimeTook",TimeTook.Ticks*100 }
+                };
+                return "{ \"index\":{} }\n"+
+                    JsonConvert.SerializeObject(dic, Formatting.None)+"\n";
+            }
+        }
+
+        static HttpClient client = new HttpClient();
+        static List<Item> items = new List<Item>();
+        static string url = "http://localhost:9200/stats/_bulk";
+        public static void Prefix(MonoBehaviour __instance, out Stopwatch __state)
+        {
+            __state = Stopwatch.StartNew();
+            //MelonLogger.Msg("Pre: " + __instance.GetType().Name);
+        }
+
+        public static void Postfix(Behaviour __instance, Stopwatch __state)
+        {
+            if (__instance == null || __state == null) return;
+            __state.Stop();
+            //save
+
+            if (items.Count > 5000)
+                {
+                    StringBuilder st = new StringBuilder();
+
+                    foreach (var item in items)
+                    {
+                        st.Append(item);
+                    }
+                    var data = new StringContent(st.ToString(), Encoding.UTF8, "application/json");
+
+                    client.PostAsync(url, data);
+                    items.Clear();
+            }
+
+
+            if (__instance is UdonBehaviour)
+            {
+                items.Add(new Item("UdonBehaviour", "", __instance.name, __state.Elapsed));
+            }
+            if (__instance is DynamicBoneController)
+            {
+                string name = __instance.transform.root.gameObject.GetComponent<VRCPlayer>()?.prop_String_0;
+                items.Add(new Item("DynamicBoneController", name, __instance.name, __state.Elapsed));
+            }
+
+            if (__instance is Camera)
+            {
+                string name = __instance.transform.root.gameObject.GetComponent<VRCPlayer>()?.prop_String_0;
+                items.Add(new Item("Camera", name, __instance.name, __state.Elapsed));          
+            }
+            //MelonLogger.Msg("Post: " +  __instance.GetType().Name);
+
+        }
+
+        static Dictionary<int, Stopwatch> cameras = new Dictionary<int, Stopwatch>();
+
+        public static void PrefixCamera(Camera cam)
+        {
+            cameras.Add(cam.gameObject.GetInstanceID(), Stopwatch.StartNew());
+        }
+        public static void PostfixCamera(Camera cam)
+        {
+            if (!cameras.ContainsKey(cam.gameObject.GetInstanceID())) return;
+            Postfix(cam, cameras[cam.gameObject.GetInstanceID()]);
+            cameras.Remove(cam.gameObject.GetInstanceID());
+        }
+    }
+}
---
+++ UnityPerfMon/PerfMon.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Diagnostics;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityPerfMon;
+using VRC;
+using VRC.Udon;
+
+[assembly: MelonInfo(typeof(PerfMon), "UnityPerfMon", "0.0.1", "Eric")]
+[assembly: MelonGame]
+
+namespace UnityPerfMon
+{
+     //[HarmonyPatch(typeof(MonoBehaviour), "Update")]
+    public class PerfMon : MelonMod
+    {
+        
+
+        public override void OnApplicationStart()
+        {
+
+
+            MelonLogger.Msg("Patching Methods (Pre/Post)");
+            HarmonyInstance.Patch(
+                    typeof(UdonBehaviour).GetMethod(nameof(UdonBehaviour.PostLateUpdate)),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Prefix))),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Postfix))));
+            HarmonyInstance.Patch(
+                    typeof(UdonBehaviour).GetMethod(nameof(UdonBehaviour.ManagedUpdate)),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Prefix))),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Postfix))));
+
+            HarmonyInstance.Patch(
+                    typeof(UdonBehaviour).GetMethod(nameof(UdonBehaviour.ManagedLateUpdate)),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Prefix))),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Postfix))));
+
+            //HarmonyInstance.Patch(
+            //        typeof(DynamicBone).GetMethod(nameof(DynamicBone.Method_Private_Void_Single_Boolean_0)),
+            //        new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Prefix))),
+            //        new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Postfix))));
+
+
+
+            HarmonyInstance.Patch(
+                    typeof(DynamicBoneController).GetMethod(nameof(DynamicBoneController.LateUpdate)),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Prefix))),
+                    new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.Postfix))));
+
+            HarmonyInstance.Patch(
+                     typeof(Camera).GetMethod(nameof(Camera.FireOnPreRender), new Type[] { typeof(Camera) }),
+                     new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.PrefixCamera), new Type[] { typeof(Camera) })));
+
+
+            HarmonyInstance.Patch(
+                     typeof(Camera).GetMethod(nameof(Camera.FireOnPostRender), new Type[] { typeof(Camera) }),
+                     new HarmonyMethod(AccessTools.Method(typeof(Hooker), nameof(Hooker.PostfixCamera), new Type[] { typeof(Camera) })));
+
+
+
+            MelonLogger.Msg("Finished Methods (Pre/Post)");
+        }
+
+        
+    }
+}
